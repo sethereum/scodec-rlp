c07a4df75fd171daab3bdf6172f0e3fdf5c13f2b
Lance Arlaus <lance.arlaus@gmail.com>
2015-11-29 09:52:25 -0500

    Wire protocol WIP

--- src/main/scala/com/github/sethereum/protocol/Packet.scala
+++ src/main/scala/com/github/sethereum/protocol/Packet.scala
@@ -4,16 +4,37 @@ import scodec.codecs._
 import com.github.sethereum.rlp._
 
 
-sealed trait PacketType
-case object Ping extends PacketType
-case object Pong extends PacketType
-case object FindNeighbors extends PacketType
-case object Neighbors extends PacketType
+sealed abstract class PacketType(val id: Int)
+object PacketType {
 
-case class Packet(hash: String)
+  case object Ping          extends PacketType(1)
+  case object Pong          extends PacketType(2)
+  case object FindNeighbors extends PacketType(3)
+  case object Neighbors     extends PacketType(4)
+
+  val map = Seq(Ping, Pong, FindNeighbors, Neighbors).map(v => (v -> v.id)).toMap
+}
+
+sealed abstract class Packet {
+  val hash: String
+}
+object Packet {
+}
+case class Ping(hash: String) extends Packet
+case class Pong(hash: String) extends Packet
+case class FindNeighbors(hash: String) extends Packet
+case class Neighbors(hash: String) extends Packet
+
 
 object Packet {
 
-  val codec = ()
+  val baseCodec = ("hash" | bits(512))
+
+  val codec = (
+    ("packet type" | mappedEnum(uint8, PacketType.map))
+  )
+
+  val codec2 = discriminated[Packet].by(uint8)
+    .\ (1) { case p: Ping => p } ((("hash" | bits(512))).as[Ping])
 
 }
