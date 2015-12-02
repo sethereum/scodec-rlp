b9ec7fd22cdb1d3325a60afbd17ddfca16291220
Lance Arlaus <lance.arlaus@gmail.com>
2015-12-02 12:02:12 -0500

    EVM operations WiP

--- src/main/scala/com/github/sethereum/evm/package.scala
+++ src/main/scala/com/github/sethereum/evm/package.scala
@@ -8,7 +8,15 @@ package object evm {
   object EvmWord {
     val SIZE = 256
     val BYTES = SIZE / 8
+    val ZERO = Array.fill(32)(0.toByte)
   }
 
+  type EvmStorage = Map[EvmStorage.Key, EvmStorage.Value]
+  object EvmStorage {
+    type Key = EvmWord
+    type Value = EvmWord
+
+    val empty = Map.empty[Key, Value].withDefaultValue(EvmWord.ZERO)
+  }
 
 }
