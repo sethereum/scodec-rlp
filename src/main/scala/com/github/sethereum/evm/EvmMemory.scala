ffe27ebe72c38ac0f05f53008e8321cf0b1946a6
Lance Arlaus <lance.arlaus@gmail.com>
2015-12-07 17:47:44 -0500

    EVM operations WiP

--- src/main/scala/com/github/sethereum/evm/EvmMemory.scala
+++ src/main/scala/com/github/sethereum/evm/EvmMemory.scala
@@ -1,10 +1,30 @@
 package com.github.sethereum.evm
 
-import scala.collection.immutable.SortedMap
+import java.math.BigInteger
+
+import com.github.sethereum.evm.EvmWordConversions._
+
 import scala.util.Try
 
 trait EvmMemoryOps[T <: EvmMemoryOps[T]] { this: T =>
-  def memLoad(key: EvmWord): Try[EvmWord]
+  def size: EvmWord
+  def memLoad(begin: EvmWord): Try[(EvmWord, T)]
+  def memStore(begin: EvmWord, value: EvmWord): Try[T]
+  def memStore(begin: EvmWord, value: Byte): Try[T]
 }
 
-//case class EvmMemory(values: SortedMap[EvmWord, EvmWord] = SortedMap.empty, bytes: Int = 0)
+case class EvmMemory private (values: Map[EvmWord, EvmWord] = Map.empty.withDefault(_ => EvmWord.ZERO), end: EvmWord = EvmWord.ZERO) extends EvmMemoryOps[EvmMemory] {
+  import EvmMemory._
+  
+  override def size: EvmWord = (end: BigInteger).multiply(EvmWordSize)
+
+  override def memLoad(begin: EvmWord): Try[(EvmWord, EvmMemory)] = ???
+
+  override def memStore(begin: EvmWord, value: EvmWord): Try[EvmMemory] = ???
+
+  override def memStore(begin: EvmWord, value: Byte): Try[EvmMemory] = ???
+}
+
+object EvmMemory {
+  val EvmWordSize: BigInteger = BigInteger.valueOf(EvmWord.BYTES)
+}
